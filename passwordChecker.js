d3bc891f43979bc75229eca5376bc71358b8e493
vandenberghen <ethan.vandenberghen@student.vinci.be>
2025-01-17 16:50:58 +0100

    Refactor: add function charactersChecket

--- passwordChecker.js
+++ passwordChecker.js
@@ -1,8 +1,12 @@
 function passwordChecker(password) {
+    return charactersChecker(password);
+}
+
+function charactersChecker(password) {
     if(password.length < 8) {
         return false;
     }
     return true;
 }
 
-module.exports = {passwordChecker};
+module.exports = {passwordChecker, charactersChecker};
--- spec/passwordChecker.test.js
+++ spec/passwordChecker.test.js
@@ -1,24 +1,24 @@
-const {passwordChecker} = require('../passwordChecker');
+const {passwordChecker,charactersChecker} = require('../passwordChecker');
 
 describe('Password validation', () => {
     it('should return false if password is less than 8 characters', () => {
         const password = 'short';
 
-        const response = passwordChecker(password);
+        const response = charactersChecker(password);
 
         expect(response).toBe(false);
     });
     it('should return true if password is  8 characters', () => {
         const password = 'shortiyto';
 
-        const response = passwordChecker(password);
+        const response = charactersChecker(password);
 
         expect(response).toBe(true);
     });
     it('should return true if password is more than 8 characters', () => {
         const password = 'shortiytoto';
 
-        const response = passwordChecker(password);
+        const response = charactersChecker(password);
 
         expect(response).toBe(true);
     });
